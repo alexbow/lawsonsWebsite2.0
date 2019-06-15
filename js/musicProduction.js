b66703b95537bd3085cd8ff2ef6b52ff0ff9ec18
unknown <caffinefree19@yahoo.com>
2019-06-15 13:02:07 -0500

    Added waveforms into all the submenus

--- js/musicProduction.js
+++ js/musicProduction.js
@@ -18,6 +18,7 @@
 
     function closeOtherTabs(id) {
       var activeTab = document.getElementsByClassName("collapsible active")[0];
+      console.log(id)
       if(activeTab){
         activeTab.click()
       }
@@ -71,4 +72,179 @@
       });
 
       addictedToYou.load('./resources/Orchestration.MP3');
+    }
+
+    function noose() {
+      var noose = WaveSurfer.create({
+          // Use the id or class-name of the element you created, as a selector
+          container: '#noose',
+          // The color can be either a simple CSS color or a Canvas gradient
+          progressColor: '#FF3E43',
+          cursorColor: '#FF3E43',
+          waveColor: 'rgba(254, 0, 6, 0.6)',
+          // This parameter makes the waveform look like SoundCloud's player
+          barWidth: 3,
+          hideScrollbar: true,
+          fillParent: true
+      });
+
+      noose.on('loading', function (percents) {
+          document.getElementById('progress').value = percents;
+      });
+
+      noose.on('ready', function (percents) {
+          document.getElementById('progress').style.display = 'none';
+      });
+
+      noose.load('./resources/Orchestration.MP3');
+    }
+
+    function allison() {
+      var allison = WaveSurfer.create({
+          // Use the id or class-name of the element you created, as a selector
+          container: '#allison',
+          // The color can be either a simple CSS color or a Canvas gradient
+          progressColor: '#FF3E43',
+          cursorColor: '#FF3E43',
+          waveColor: 'rgba(254, 0, 6, 0.6)',
+          // This parameter makes the waveform look like SoundCloud's player
+          barWidth: 3,
+          hideScrollbar: true,
+          fillParent: true
+      });
+
+      allison.on('loading', function (percents) {
+          document.getElementById('progress').value = percents;
+      });
+
+      allison.on('ready', function (percents) {
+          document.getElementById('progress').style.display = 'none';
+      });
+
+      allison.load('./resources/Orchestration.MP3');
+    }
+
+    function orange() {
+      var orange = WaveSurfer.create({
+          // Use the id or class-name of the element you created, as a selector
+          container: '#orange',
+          // The color can be either a simple CSS color or a Canvas gradient
+          progressColor: '#FF3E43',
+          cursorColor: '#FF3E43',
+          waveColor: 'rgba(254, 0, 6, 0.6)',
+          // This parameter makes the waveform look like SoundCloud's player
+          barWidth: 3,
+          hideScrollbar: true,
+          fillParent: true
+      });
+
+      orange.on('loading', function (percents) {
+          document.getElementById('progress').value = percents;
+      });
+
+      orange.on('ready', function (percents) {
+          document.getElementById('progress').style.display = 'none';
+      });
+
+      orange.load('./resources/Orchestration.MP3');
+    }
+
+    function snowmen() {
+      var snowmen = WaveSurfer.create({
+          // Use the id or class-name of the element you created, as a selector
+          container: '#snowmen',
+          // The color can be either a simple CSS color or a Canvas gradient
+          progressColor: '#FF3E43',
+          cursorColor: '#FF3E43',
+          waveColor: 'rgba(254, 0, 6, 0.6)',
+          // This parameter makes the waveform look like SoundCloud's player
+          barWidth: 3,
+          hideScrollbar: true,
+          fillParent: true
+      });
+
+      snowmen.on('loading', function (percents) {
+          document.getElementById('progress').value = percents;
+      });
+
+      snowmen.on('ready', function (percents) {
+          document.getElementById('progress').style.display = 'none';
+      });
+
+      snowmen.load('./resources/Orchestration.MP3');
+    }
+
+    function openingNight() {
+      var openingNight = WaveSurfer.create({
+          // Use the id or class-name of the element you created, as a selector
+          container: '#openingNight',
+          // The color can be either a simple CSS color or a Canvas gradient
+          progressColor: '#FF3E43',
+          cursorColor: '#FF3E43',
+          waveColor: 'rgba(254, 0, 6, 0.6)',
+          // This parameter makes the waveform look like SoundCloud's player
+          barWidth: 3,
+          hideScrollbar: true,
+          fillParent: true
+      });
+
+      openingNight.on('loading', function (percents) {
+          document.getElementById('progress').value = percents;
+      });
+
+      openingNight.on('ready', function (percents) {
+          document.getElementById('progress').style.display = 'none';
+      });
+
+      openingNight.load('./resources/Orchestration.MP3');
+    }
+
+    function matador() {
+      var matador = WaveSurfer.create({
+          // Use the id or class-name of the element you created, as a selector
+          container: '#matador',
+          // The color can be either a simple CSS color or a Canvas gradient
+          progressColor: '#FF3E43',
+          cursorColor: '#FF3E43',
+          waveColor: 'rgba(254, 0, 6, 0.6)',
+          // This parameter makes the waveform look like SoundCloud's player
+          barWidth: 3,
+          hideScrollbar: true,
+          fillParent: true
+      });
+
+      matador.on('loading', function (percents) {
+          document.getElementById('progress').value = percents;
+      });
+
+      matador.on('ready', function (percents) {
+          document.getElementById('progress').style.display = 'none';
+      });
+
+      matador.load('./resources/Orchestration.MP3');
+    }
+
+    function quixotic() {
+      var quixotic = WaveSurfer.create({
+          // Use the id or class-name of the element you created, as a selector
+          container: '#quixotic',
+          // The color can be either a simple CSS color or a Canvas gradient
+          progressColor: '#FF3E43',
+          cursorColor: '#FF3E43',
+          waveColor: 'rgba(254, 0, 6, 0.6)',
+          // This parameter makes the waveform look like SoundCloud's player
+          barWidth: 3,
+          hideScrollbar: true,
+          fillParent: true
+      });
+
+      quixotic.on('loading', function (percents) {
+          document.getElementById('progress').value = percents;
+      });
+
+      quixotic.on('ready', function (percents) {
+          document.getElementById('progress').style.display = 'none';
+      });
+
+      quixotic.load('./resources/Orchestration.MP3');
     }
